e381b000de694f172b7d0c2131c59187fb4ae5b4
shawn-flunge <echun1234@gmail.com>
2025-04-16 22:09:49 +0900

    코스 상세에서 bnb 클릭 시 뒤로가기

--- lib/src/pages/main/main_scaffold.dart
+++ lib/src/pages/main/main_scaffold.dart
@@ -4,6 +4,7 @@
 import 'package:design_system/widget.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:rooty/src/pages/router.enum.dart';
 
 class RootyMainScaffold extends StatelessWidget {
 
@@ -21,7 +22,13 @@ class RootyMainScaffold extends StatelessWidget {
       bottomNavigationBar: RTBnb(
         shellIndex: shell.currentIndex,
         onTapChanged: (index) {
-          shell.goBranch(index);
+
+          if(index == 1 && GoRouter.of(context).state.name == Routes.course.name) {
+            context.pop();
+            // context.goNamed(Routes.courses.name);
+          } else {
+            shell.goBranch(index);
+          }
         },
       ),
     );
