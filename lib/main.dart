6cf8f8739eb4ecd7b32388d842bac623cb3e40cc
shawn-flunge <echun1234@gmail.com>
2025-04-21 20:40:47 +0900

    provider 관련 패키지 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:rooty/src/app.dart';
 
 void main() {
   initializeDateFormatting();
-  runApp(RootyApp());
+  runApp(
+    ProviderScope(child: RootyApp())
+  );
 }
