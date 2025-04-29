57bb2241bd6d4f7bf2e046db315bc3936c9a8b33
shawn-flunge <echun1234@gmail.com>
2025-04-29 22:06:41 +0900

    플랫폼 별 firebase 세팅 대응

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:rooty/firebase_options.dart';
 import 'package:rooty/src/app.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
   initializeDateFormatting();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ProviderScope(child: RootyApp())
   );
