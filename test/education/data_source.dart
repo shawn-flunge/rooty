35317a836b175da955a26cf0638524e562f75d85
shawn-flunge <echun1234@gmail.com>
2025-05-07 21:57:11 +0900

    data_source 패키지 작동 여부 테스트

---
+++ test/education/data_source.dart
@@ -0,0 +1,23 @@
+
+
+
+
+import 'package:data_source/data_source.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rooty/src/supabase_options.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+
+  setUp(() async{
+    SharedPreferences.setMockInitialValues({});
+    await SupabaseOptions.initialize();
+  });
+
+  test('test education data source works', () async {
+
+    final source = EducationDataSource();
+    await source.getRootList();
+  });
+
+}
