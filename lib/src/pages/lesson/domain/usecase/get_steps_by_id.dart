e4315cd409c3ba32f8fa9bbc042aedc83b7ff6a3
shawn-flunge <echun1234@gmail.com>
2025-05-19 22:41:16 +0900

    불필요한 참조 제거

--- lib/src/pages/lesson/domain/usecase/get_steps_by_id.dart
+++ lib/src/pages/lesson/domain/usecase/get_steps_by_id.dart
@@ -1,22 +1,7 @@
 
-
-
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:rooty/src/core/usecase.dart';
-import 'package:rooty/src/pages/lesson/data/resository/step.dart';
 import 'package:rooty/src/pages/lesson/domain/repository/step_repository.dart';
 
-// part 'get_steps_by_id.g.dart';
-//
-// @riverpod
-// class GetStepsById extends _$GetStepsById {
-//
-//   @override
-//   Future<dynamic> build(int lessonId) async{
-//     // final repository = ref.read(stepRepositoryProvider);
-//     return repository.getSteps(lessonId);
-//   }
-// }
 
 class GetStepsById extends UseCase<dynamic, int> {
 
