b26a3f4e6a38af22a19a1f7029aff9bde8fbe38f
shawn-flunge <echun1234@gmail.com>
2025-05-20 20:09:13 +0900

    StepEntity 적용

--- lib/src/pages/lesson/domain/usecase/get_steps_by_id.dart
+++ lib/src/pages/lesson/domain/usecase/get_steps_by_id.dart
@@ -1,16 +1,17 @@
 
 import 'package:rooty/src/core/usecase.dart';
+import 'package:rooty/src/pages/lesson/domain/entity/step.dart';
 import 'package:rooty/src/pages/lesson/domain/repository/step_repository.dart';
 
 
-class GetStepsById extends UseCase<dynamic, int> {
+class GetStepsById extends UseCase<List<StepEntity>, int> {
 
   final StepRepository repository;
 
   GetStepsById(this.repository);
 
   @override
-  Future call(int param) async{
+  Future< List<StepEntity> > call(int param) async{
     return repository.getSteps(param);
   }
 
