fdd14ceee12b7f2e931ac931bed3ef706898657b
shawn-flunge <echun1234@gmail.com>
2025-05-30 17:27:44 +0900

    bnb에 haptic feedback 추가

--- lib/src/pages/main/main_scaffold.dart
+++ lib/src/pages/main/main_scaffold.dart
@@ -4,6 +4,7 @@
 import 'package:design_system/layout.dart' show PlatformLayout;
 import 'package:design_system/widget.dart' show RTBnb;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import 'package:rooty/src/pages/router.enum.dart';
 
@@ -25,6 +26,7 @@ class RootyMainScaffold extends StatelessWidget {
         bottomNavigationBar: RTBnb(
           shellIndex: shell.currentIndex,
           onTapChanged: (index) {
+            HapticFeedback.selectionClick();
 
             if(index == 1 && GoRouter.of(context).state.name == Routes.course.name) {
               context.pop();
