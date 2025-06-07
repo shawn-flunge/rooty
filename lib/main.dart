6b6e09a74d04cb0279ad6b2526d776e9be4b9c79
shawn-flunge <echun1234@gmail.com>
2025-06-07 16:46:26 +0900

    일단 똑같게

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:data_source/data_source.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,17 +5,18 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:rooty/firebase_options.dart';
 import 'package:rooty/src/app.dart';
 import 'package:rooty/src/domains/app_setting/provider/provider.dart';
+import 'package:rooty/supabase_options.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
 
   initializeDateFormatting();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+      options: DefaultFirebaseOptions.currentPlatform,
+      name: 'production'
   );
-  /// local 개발 시 호출
-  dataConnectEmulatorInit();
 
+  await SupabaseOptions.initialize();
   final setting = await AppSettingNotifier.initialize();
 
   runApp(
