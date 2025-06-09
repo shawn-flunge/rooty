07010c1cb274bfcaa5e003de9520864ae9b5101b
shawn-flunge <echun1234@gmail.com>
2025-06-09 15:20:49 +0900

    뒤로가기 처리

--- lib/src/pages/lesson/presentation/page/step_list.dart
+++ lib/src/pages/lesson/presentation/page/step_list.dart
@@ -1,8 +1,11 @@
 
+import 'package:design_system/color.dart';
 import 'package:design_system/widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:rooty/src/localizations/texts.dart';
+import 'package:rooty/src/localizations/widget.dart';
 import 'package:rooty/src/pages/lesson/domain/entity/step.dart';
 import 'package:rooty/src/pages/lesson/presentation/provider/provider.dart';
 import 'package:rooty/src/pages/lesson/presentation/widget/step_view.dart';
@@ -72,7 +75,13 @@ class _LessonStepListPageState extends ConsumerState<LessonStepListPage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: RTAppBarPlain(),
+      appBar: RTAppBarPlain(
+        onClosed: () async{
+          final wannaGoBack = await showConfirmDialog(context) ?? false;
+
+          return wannaGoBack;
+        },
+      ),
       body: PageView.builder(
         controller: _controller,
         itemCount: _progress.length,
@@ -115,6 +124,75 @@ class _LessonStepListPageState extends ConsumerState<LessonStepListPage> {
       //
       //   },
       // ),
+    );
+  }
+
+
+  /// todo: 공통화
+  Future<bool?> showConfirmDialog<T>(BuildContext context) async{
+
+    return showDialog<bool?>(
+        context: context,
+        builder: (c) {
+          return AlertDialog(
+            backgroundColor: context.colors.modalBackground,
+            actionsAlignment: MainAxisAlignment.center,
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                LocalizedText(
+                  'lesson_exit_modal_title',
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: context.colors.text,
+                      fontVariations: [
+                        FontVariation.weight(400)
+                      ]
+                  ),
+                ),
+                const SizedBox(height: 8,),
+                LocalizedText(
+                  'lesson_exit_modal_description',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: context.colors.text,
+                      fontVariations: [
+                        FontVariation.weight(320)
+                      ]
+                  ),
+                ),
+                const SizedBox(height: 16,),
+                Row(
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      fit: FlexFit.tight,
+                      child: RTConfirmButton.small(
+                        text: RootyTexts.of(context).get('lesson_exit_modal_continue')?.text ?? '',
+                        onPressed: () {
+                          context.pop(false);
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 8,),
+                    Flexible(
+                      flex: 1,
+                      fit: FlexFit.tight,
+                      child: RTConfirmButton.small(
+                        text: RootyTexts.of(context).get('exit')?.text ?? '',
+                        textColor: context.colors.text,
+                        backgroundColor: context.colors.lightGrey,
+                        onPressed: () {
+                          context.pop(true);
+                        },
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        }
     );
   }
 
