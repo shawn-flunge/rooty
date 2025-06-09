f30e2a0170ff83dadefa51ed1ad69dead54cb841
shawn-flunge <echun1234@gmail.com>
2025-06-09 14:35:55 +0900

    firebase_crashlytics 추가

--- lib/running/production.dart
+++ lib/running/production.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -15,6 +16,7 @@ void main() async{
     options: DefaultFirebaseOptions.currentPlatform,
     name: 'production'
   );
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
 
   await SupabaseOptions.initialize();
   final setting = await AppSettingNotifier.initialize();
