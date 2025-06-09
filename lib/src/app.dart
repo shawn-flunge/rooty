53d3cc790a27e2444938b4fce68d1cb0a6f49a53
shawn-flunge <echun1234@gmail.com>
2025-06-09 16:28:30 +0900

    att 대응

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,11 @@
 
 
 import 'package:app_tracking_transparency/app_tracking_transparency.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:design_system/theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:rooty/src/domains/app_setting/provider/provider.dart';
 import 'package:rooty/src/domains/app_setting/provider/state.dart';
 import 'package:rooty/src/localizations/delegate.dart';
@@ -24,8 +26,17 @@ class _RootyAppState extends ConsumerState<RootyApp> {
   void initState() {
     super.initState();
     WidgetsFlutterBinding.ensureInitialized().addPostFrameCallback((_) async {
-      final status = await AppTrackingTransparency.requestTrackingAuthorization();
 
+      if(defaultTargetPlatform == TargetPlatform.iOS) {
+        final TrackingStatus status = await AppTrackingTransparency.trackingAuthorizationStatus;
+        while (status == TrackingStatus.notDetermined) {
+          await Future.delayed(const Duration(seconds: 1));
+          final TrackingStatus newStatus = await AppTrackingTransparency.requestTrackingAuthorization();
+          if (newStatus != TrackingStatus.notDetermined) {
+            break;
+          }
+        }
+      }
     });
   }
 
